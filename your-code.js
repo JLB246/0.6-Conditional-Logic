8bb159c68a67e3b48a61d2c1169f8ee2bfa5fd46
JLB246 <65634399+JLB246@users.noreply.github.com>
2020-07-29 14:53:47 -0400

    Create your-code.js

---
+++ your-code.js
@@ -0,0 +1,12 @@
+var answer = prompt('What day of the week is today?');
+if (answer === 'Thursday'){
+      console.log('Just one more day until Friday'+'!');
+      
+}
+else if (answer === 'Friday'){
+      console.log('Party time'+'!');
+      
+}
+else{
+      console.log('Happy'+ answer + '!');
+}
